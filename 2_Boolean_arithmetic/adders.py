dc2a02d1cfa77603254b07510e4a3c9e305366c6
Daniel Brown <pi_anyone@hotmail.com>
2015-03-05 21:05:24 +0100

    add skeleton functions for adders

--- 2_Boolean_arithmetic/adders.py
+++ 2_Boolean_arithmetic/adders.py
@@ -1,2 +1,15 @@
 from gates import *
 from mutlibit_gates import *
+
+
+def HalfAdder(a,b):
+    return (0,0)
+
+def FullAdder(a,b,c):
+    return (0,0,0)
+
+def Add16(a16,b16):
+    return a16
+
+def Inc16(a16):
+    return a16
